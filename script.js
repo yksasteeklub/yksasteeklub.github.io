d997532d7aa50f9c5f5e183b7a669e0b453e3b19
SharpSX <71710712+SharpSX@users.noreply.github.com>
2021-06-21 18:29:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,42 +11,37 @@ xv=yv=0;
 trail=[];
 tail = 5;
 function game() {
-    px+=xv;
-    py+=yv;
-    if(px<0) {
-        px= tc-1;
-    }
-    if(px>tc-1) {
-        px= 0;
-    }
-    if(py<0) {
-        py= tc-1;
-    }
-    if(py>tc-1) {
-        py= 0;
-    }
-    ctx.fillStyle="black";
-    ctx.fillRect(0,0,canv.width,canv.height);
+        px += xv;
+    py += yv;
+    if (px < 0) px = tc - 1;
+    if (px > tc - 1) px = 0;
+    if (py < 0) py = tc - 1;
+    if (py > tc - 1) py = 0;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canv.width, canv.height);
 
-    ctx.fillStyle="lime";
-    for(var i=0;i<trail.length;i++) {
-        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
-        if(trail[i].x==px && trail[i].y==py) {
+    ctx.fillStyle = "lime";
+    for (var i = 0; i < trail.length; i++) {
+        ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
+        if (trail[i].x == px && trail[i].y == py) {
             tail = 5;
         }
     }
-    trail.push({x:px,y:py});
-    while(trail.length>tail) {
-    trail.shift();
+    trail.push({
+        x: px,
+        y: py
+    });
+    while (trail.length > tail) {
+        trail.shift();
     }
 
-    if(ax==px && ay==py) {
+    if (ax == px && ay == py) {
         tail++;
-        ax=Math.floor(Math.random()*tc);
-        ay=Math.floor(Math.random()*tc);
+        ax = Math.floor(Math.random() * tc);
+        ay = Math.floor(Math.random() * tc);
     }
-    ctx.fillStyle="red";
-    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+    ctx.fillStyle = "red";
+    ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
 }
 function keyPush(evt) {
     switch(evt.keyCode) {
