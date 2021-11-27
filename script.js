2ffeae157aacc28795f1303f95151c39a3c33c7f
Tonttu37 <71811815+Tonttu37@users.noreply.github.com>
2021-11-27 18:16:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,21 @@ function keyPush(evt) {
             break;
     }
 }
+
+let data = [
+    {
+        name: "Negat",
+        avatar: "https://i1.sndcdn.com/artworks-xld5lLClKqURslHF-XgFUzw-t500x500.jpg",
+        review: "Vitun paska serveri, kokoajan holokausteja, olen mutella aina. 0/5!"
+    },
+    {
+        name: "Moderaattori Sharp",
+        avatar: "https://www.packsmart.com//img/product/Frontal_View-B.jpg",
+        review: "Itsehän olen moderaattori kyseisellä serverillä, joten oma mp on 5/5"
+        
+    }
+        ];
+
+let details = data.forEach(name => {
+    console.log(name)
+});
