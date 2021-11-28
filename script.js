e4161ee0d550057558242a485434a9e82f17f946
Tonttu37 <71811815+Tonttu37@users.noreply.github.com>
2021-11-28 19:27:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,6 @@ let data = [
         review: '"Itsehän olen moderaattori kyseisellä serverillä, joten oma mp on 5/5"'
     },
     {
-        
         name: 'ETKSA_',
         avatar: 'https://lh3.googleusercontent.com/57bqYcHBeLzBoM5JYqKSz05B6w4XBIfxOVv0JnqH7xZwRo5epxs8d01gJweQ59s_8TLS=s85',
         review: '"Vitun kyrvät vetäkää käteen teiän pingien kanssa!!"'
