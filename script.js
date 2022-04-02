c0824d72f662221140dc4712d1586fedd53c58cb
Tonttu37 <secondlol491@gmail.com>
2022-04-02 15:23:07 +0300

    pitää kusettaa ihmisii et olis 2020 näyttä vanhalt

--- script.js
+++ script.js
@@ -78,6 +78,6 @@ function showPerson(person) {
 }
 } else {
     window.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('.yearNow').innerHTML = "Copyright © 1ST CLUB 2021 - " + date + " All rights reserved <br><br>design by SharpSX 2021";
+    document.querySelector('.yearNow').innerHTML = "Copyright © 1ST CLUB 2020 - " + date + " All rights reserved <br><br>design by SharpSX 2021";
     })
 }
